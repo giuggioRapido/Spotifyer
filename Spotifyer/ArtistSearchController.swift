18182b1392720896731707043682540447160f3b
Christopher Shea <Chris@Christophers-MacBook-Pro.local>
2016-06-14 14:18:13 -0400

    Add ArtistSearchController.swift and RequestManager.swift

---
+++ Spotifyer/ArtistSearchController.swift
@@ -0,0 +1,14 @@
+//
+//  ArtistSearchController.swift
+//  Spotifyer
+//
+//  Created by Christopher Shea on 6/14/16.
+//  Copyright © 2016 Christopher Shea. All rights reserved.
+//
+
+import UIKit
+
+class ArtistSearchController: UISearchController {
+
+    
+}
---
+++ Spotifyer/RequestManager.swift
@@ -0,0 +1,9 @@
+//
+//  RequestManager.swift
+//  Spotifyer
+//
+//  Created by Christopher Shea on 6/14/16.
+//  Copyright © 2016 Christopher Shea. All rights reserved.
+//
+
+import Foundation
