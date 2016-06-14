6a24a00c7bb4b4646555175658d69dda0fe5771a
Christopher Shea <Chris@Christophers-MacBook-Pro.local>
2016-06-14 14:23:07 -0400

    Create Artist model struct

---
+++ Spotifyer/Artist.swift
@@ -0,0 +1,15 @@
+//
+//  Artist.swift
+//  Spotifyer
+//
+//  Created by Christopher Shea on 6/14/16.
+//  Copyright © 2016 Christopher Shea. All rights reserved.
+//
+
+import Foundation
+
+struct Artist {
+    let name: String
+    let biography: String
+    let photoURL: NSURL
+}
